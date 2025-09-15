30e3a8548d63a8fe5c222768aaa9b3f9ff0f5891
seethaladevi2024-cpu <seethaladevi2024@gmail.com>
2025-09-15 23:02:18 +0530

    First Java Program: Calculator

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Calculator {
+    public static void main(String[] args){
+        Scanner object = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double number1 = object.nextDouble();
+        object.nextLine();
+        System.out.print("Enter second number: ");
+        double number2 = object.nextDouble();
+        object.nextLine();
+        System.out.println("1.add\n2.sub\n3.mul\n4.div\n");
+        System.out.print("Enter your choice:");
+        int operation = object.nextInt();
+        if(operation==1){
+            System.out.printf("%f + %f = %f",number1,number2,number1 + number2);
+        }
+            else if(operation==2){
+            System.out.printf("%f - %f = %f",number1,number2,number1 - number2);
+        }
+        else if(operation==3){
+            System.out.printf("%f * %f = %f",number1,number2,number1 * number2);
+        }
+        else if(operation==4){
+            if(number2==0){
+                System.out.println("Error can't divide with zero");
+            }
+            else{
+                System.out.printf("%f / %f = %f",number1,number2,number1 % number2);
+                object.close();
+            }
+            
+        }
+        
+
+    }
+    
+}
